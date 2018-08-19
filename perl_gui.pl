173998093eac251a3ad5c1e5736838147db6f510
anandw54 <42508719+anandw54@users.noreply.github.com>
2018-08-19 12:27:08 +0530

    Adding latest changes

--- perl_gui.pl
+++ perl_gui.pl
@@ -1,25 +1,294 @@
+#1. Install padre perl in host computer
+#2. install tk module
+#C:\Users\palaskar\Downloads\Tk-804.034\Tk-804.034
+#Go to Downloads where  module is downloaded in its tar version
+#Extract it
+#cd C:\Users\palaskar\Downloads\Tk-804.034\Tk-804.034
+#perl Makefile.PL
+#dmake
+#dmake test
+#dmake install
+#Once the module is installed, go sample scripts or examples from command window and run those test scripts using "perl script_nmae:
+#If those tests run successfully, place your new scripts along with those scripts and go ahead.
+#3. Update device ids and their IPs 
+#4. convert program to executable
+#cpan -i PAR::Packer
+#pp -o example example.pl
+
+
 use Tk;
 use Tk::JPEG;
 use Tk::Pane;
-use strict;
-use warnings;
+use IO::Socket::INET;
 
+my @IP_Address;
+$IP_Address[0] = '192.168.1.7';	#IP input from user
+$IP_Address[1] = '192.168.1.3';   	#IP input from user
+$IP_Address[2] = '192.168.1.4';   	#IP input from user
+$IP_Address[3] = '192.168.1.6';   	#IP input from user
+$IP_Address[4] = '192.168.1.5';   	#IP input from user
+my $trig =0;
+my $num_Rpi;
+
+
+#Global Variables
 my $dir = shift || '.';
-
 chdir $dir or die "Can't go do $dir - $!\n";
+$filespec = "*.jpg";
+@files = glob $filespec ;
 
-my $filespec = "*.jpg";
-my @files = glob $filespec
-or die "No files matching $filespec in $dir !\n";
 my $ii = -1; # image index
 
+#my $dw ;
+
+my $gender = "Hide";
+my $data;
+my $scrolled;
+my $imagit;
+
+# Main Window
 my $mw = new MainWindow;
 
-my $scrolled = $mw
-    ->Scrolled( 'Pane', -scrollbars => 'osoe', -width => 640, -height => 480, )
+my $value = 0;
+
+#imag navigation
+$mw->bind( '<Prior>' => \&prev_image );
+$mw->bind( '<Up>'    => \&prev_image );
+$mw->bind( '<Left>'  => \&prev_image );
+
+$mw->bind( '<Next>'  => \&next_image );
+$mw->bind( '<Down>'  => \&next_image );
+$mw->bind( '<Right>' => \&next_image );
+
+#$imagit->bind( '<Button1-ButtonRelease>' => sub { undef $last_x } );
+#$imagit->bind( '<Button1-Motion>' => [ \&drag, Ev('X'), Ev('Y'), ] );
+
+
+#GUI Building Area
+my $frm_name = $mw -> Frame();
+$frm_name -> grid(-row=>1,-column=>1,-columnspan=>2);
+
+my $frm_but = $mw -> Frame();
+$frm_but -> grid(-row=>2,-column=>1,-columnspan=>2);
+
+my @lab, @ent, @frm_name;
+
+#create 120 camera entries
+foreach my $i (1..30) 
+{
+$lab[$i] = $frm_name -> Label(-text=>"Camera_$i:");
+$ent[$i] = $frm_name -> Entry(-width => 15, );
+$lab[$i] -> grid(-row=>$i,-column=>1);
+$ent[$i] -> grid(-row=>$i,-column=>2);      
+}
+
+foreach my $i (31..60) 
+{
+$lab[$i] = $frm_name -> Label(-text=>"Camera_$i:");
+$ent[$i] = $frm_name -> Entry(-width => 15, );
+$lab[$i] -> grid(-row=>$i-30,-column=>3);
+$ent[$i] -> grid(-row=>$i-30,-column=>4);      
+}
+
+foreach my $i (61..90) 
+{
+$lab[$i] = $frm_name -> Label(-text=>"Camera_$i:");
+$ent[$i] = $frm_name -> Entry(-width => 15, );
+$lab[$i] -> grid(-row=>$i-60,-column=>5);
+$ent[$i] -> grid(-row=>$i-60,-column=>6);      
+}
+
+foreach my $i (91..120) 
+{
+$lab[$i] = $frm_name -> Label(-text=>"Camera_$i:");
+$ent[$i] = $frm_name -> Entry(-width => 15, );
+$lab[$i] -> grid(-row=>$i-90,-column=>7);
+$ent[$i] -> grid(-row=>$i-90,-column=>8);      
+}
+
+# Buttons for various camera functions
+$frm_but->Button(-text => "Clear", -command =>\&Clear)->grid
+  ($frm_but->Button(-text => "Load", -command =>\&Load),
+   $frm_but->Button(-text => "Trigger", -command =>\&Trigger),
+   $frm_but->Button(-text => "Display", -command =>\&Display),
+   -sticky => "nsew", -padx => 10, -pady => 10, -ipadx => 10, -ipady => 10);
+
+                                              
+#Show or hide string
+my $frm_gender = $mw -> Frame();
+my $lbl_gender = $frm_gender -> Label(-text=>"String");
+my $rdb_m = $frm_gender -> Radiobutton(-text=>"Hide",  
+        -value=>"Hide",  -variable=>\$gender);
+my $rdb_f = $frm_gender -> Radiobutton(-text=>"Show",
+        -value=>"Show",-variable=>\$gender);
+
+
+#Text Area
+my $textarea = $mw -> Frame();
+my $txt = $textarea -> Text(-width=>100, -height=>3);
+my $srl_y = $textarea -> Scrollbar(-orient=>'v',-command=>[yview => $t
++xt]);
+my $srl_x = $textarea -> Scrollbar(-orient=>'h',-command=>[xview => $t
++xt]);
+$txt -> configure(-yscrollcommand=>['set', $srl_y],
+        -xscrollcommand=>['set',$srl_x]);
+
+#Geometry Management
+$lbl_gender -> grid(-row=>1,-column=>1);
+$rdb_m -> grid(-row=>1,-column=>2);
+$rdb_f -> grid(-row=>1,-column=>3);
+$frm_gender -> grid(-row=>3,-column=>1,-columnspan=>2);
+$txt -> grid(-row=>1,-column=>1);
+$srl_y -> grid(-row=>1,-column=>2,-sticky=>"ns");
+$srl_x -> grid(-row=>2,-column=>1,-sticky=>"ew");
+$textarea -> grid(-row=>5,-column=>1,-columnspan=>2);
+
+MainLoop;
+
+## Functions
+#This function will be executed when the button is pushed
+sub Clear 
+{
+
+foreach my $i (1..30) 
+{
+$ent[$i] = $frm_name -> Entry(-width => 15, -textvariable => $value);
+$ent[$i] -> grid(-row=>$i,-column=>2);      
+}
+
+foreach my $i (31..60) 
+{
+$ent[$i] = $frm_name -> Entry(-width => 15, -textvariable => $value);
+$ent[$i] -> grid(-row=>$i-30,-column=>4);      
+}
+
+foreach my $i (61..90) 
+{
+$ent[$i] = $frm_name -> Entry(-width => 15, -textvariable => $value);
+$ent[$i] -> grid(-row=>$i-60,-column=>6);      
+}
+
+foreach my $i (91..120) 
+{
+$ent[$i] = $frm_name -> Entry(-width => 15, -textvariable => $value);
+$ent[$i] -> grid(-row=>$i-90,-column=>8);      
+}
+
+$txt -> delete('1.0','end');
+
+}
+
+sub Load 
+{
+
+my $string;
+my $star = "a";
+ 
+# Create Delay String 
+foreach my $i (1..120) 
+{
+my $name = $ent[$i] -> get();
+$string = $string . $name;
+$string = $string . $star;
+
+if ($i eq 120)
+{
+$num_Rpi = $name;
+print "Rpi connected is $num_Rpi \n";
+}
+
+}
+
+if ($gender eq "Show")
+{
+$txt -> insert('end',"$string");
+}  
+   
+$data = $string;	#Delay input string from user
+my $folder = 'folder1';		#Newfolder input from user: newfolder button or input name for new folder
+
+# Start UDP socket for delay string transmission
+my $socket;
+$socket = new IO::Socket::INET(
+	PeerPort => 1234,
+	PeerAddr => '192.168.1.255',
+	Proto => 'udp'
+) or die "error in socket creation";
+
+
+
+if($socket->send($data))
+{
+print "Delay Loaded\n";
+}
+
+$socket->close();
+}
+
+sub Trigger 
+{
+	
+my $folder = "folder.$trig";		#Newfolder input from user
+chdir 'C:\Users\palaskar\Desktop' ; 
+#$dir = $folder;
+# Start UDP socket for Trigger Command transmission
+my $socket;
+$socket = new IO::Socket::INET(
+	PeerPort => 1234,
+	PeerAddr => '192.168.1.255',
+	Proto => 'udp'
+) or die "error in socket creation";
+
+if($socket->send("R"))
+{
+print "Camera Triggered\n";
+}
+
+system("TIMEOUT 8");
+# Create folder in local directory
+system("mkdir $folder");
+my $cnt = 'C:\Users\palaskar\Desktop"';
+# Download images from Raspberries
+my $temp;
+foreach my $IP (@IP_Address)
+{
+system("pscp.exe -pw raspberry pi\@$IP:\/home\/pi\/Perl_tests\/udp_receive\/I* $folder/");
+#system("pscp.exe -pw raspberry pi\@$IP:\/home\/pi\/Perl_tests\/udp_receive\/C* $folder/");
+
+$temp = $temp +1;
+if ($temp eq $num_Rpi)
+{
+break;
+}
+}
+$socket->close();
+
+$dir = "$folder/";
+#print "previous is $previous\n";
+chdir $dir or die "Can't go do $dir - $!\n";
+
+#print "after is $after\n";
+$filespec = "*.jpg";
+@files = glob $filespec;
+
+
+#chdir $cnt or die "Can't go do $cnt - $!\n";
+$trig = $trig+1;
+}
+
+
+
+sub Display 
+{
+#$dw = MainWindow->new(-title=>"Demo");
+$dw = MainWindow->new(-title=>"Demo");
+$flag = 1;
+
+$scrolled = $dw
+    ->Scrolled( 'Pane', -scrollbars => 'osoe', -width => 5184, -height => 3456, )
     ->pack( -expand => 1, -fill => 'both', );
 
-my $imagit = $scrolled
+$imagit = $scrolled
     ->Label
     ->pack( -expand => 1, -fill => 'both', );
 
@@ -29,16 +298,13 @@ my( $last_x, $last_y );
 
 my $img2;
 
-$mw->bind( '<Prior>' => \&prev_image );
-$mw->bind( '<Up>'    => \&prev_image );
-$mw->bind( '<Left>'  => \&prev_image );
+$dw->after( 1, \&next_image );
 
-$mw->bind( '<Next>'  => \&next_image );
-$mw->bind( '<Down>'  => \&next_image );
-$mw->bind( '<Right>' => \&next_image );
+MainLoop;
 
-$imagit->bind( '<Button1-ButtonRelease>' => sub { undef $last_x } );
-$imagit->bind( '<Button1-Motion>' => [ \&drag, Ev('X'), Ev('Y'), ] );
+
+
+}
 
 sub drag
 {
@@ -69,36 +335,6 @@ sub drag
     ( $last_x, $last_y ) = ( $x, $y );
 }
 
-=pod
-
-Image scaling here is designed to strike a balance
-between not wanting to scroll too much and not
-wanting to lose too much resolution by downsampling.
-The heuristic is:
-
-1. if the image fits within the scrolled pane in one
-or both dimensions (that is, only zero or one scrollbar
-would be shown), no downsampling is done.
-
-2. otherwise (i.e. if two scrollbars would be needed),
-the downsampling factor is incremented (from 1) until
-the downsampling factor is incremented (from 1) until
-condition #1 (above) is met.
-
-(Of course, we don't actually increment and check like
-that; we calculate the desired factor algebraically.)
-
-This way, when you do have to scroll, it will often be
-on one axis only; and the distance you'll have to 
-scroll will be minimized (or rather, optimized).
-
-Another approach would be to downsample the picture
-sufficiently such that the image always fits entirely
-within the pane, and scrolling won't be necessary, but
-I'd rather give minimization of resolution loss 
-slightly more weight than eliminating the need to scroll.
-
-=cut
 
     sub factor
     {
@@ -115,8 +351,8 @@ slightly more weight than eliminating the need to scroll.
 sub show_image
 {
     my $imgfile = $files[$ii];
-    $mw->configure( -title => "($ii) - - - - - - -" );
-    my $img1 = $mw->Photo( 'fullscale',
+    $dw->configure( -title => "($ii) - - - - - - -" );
+    my $img1 = $dw->Photo( 'fullscale',
         -format => 'jpeg',
         -file => $imgfile,
     );
@@ -125,19 +361,24 @@ sub show_image
     # much control as you do when copying one image to another,
     # and some of the things we need to do we can only do
  # during copy, not reading.
+ 
+
     my $factor = min(
         factor( $img1->width, $scrolled->width ),
         factor( $img1->height, $scrolled->height ),
     );
-    $img2 = $mw->Photo( 'resized' );
-    $img2->copy( $img1, -shrink, -subsample => $factor, $factor );
+
+    $img2 = $dw->Photo( 'resized' );
+    $img2->copy( $img1, -shrink, -subsample => 3, 3 );
     $imagit->configure(
         -image => 'resized',
         -width => $img2->width,
         -height => $img2->height,
     );
-    $mw->configure( -title => "($ii) $imgfile" );
+    $dw->configure( -title => "($ii) $imgfile" );
 }
+
+
 
 sub prev_image
 {
@@ -150,7 +391,3 @@ sub next_image
     $ii = ( $ii + 1 ) % @files;
     show_image();
 }
-
-$mw->after( 1, \&next_image );
-
-MainLoop;
